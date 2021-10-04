591ed0f0fd1c4392d71f1503372aebbbe2a1e92e
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2021-10-04 15:45:21 +0530

    sidde timeline added

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const pageScrollButton = document.querySelector(".page-scroll-button");
+const timelineMarker=document.querySelector(".timeline-marker");
+
+const calculateTopMargin=()=>{
+  var topMargin=(document.querySelector(".year-wrapper").clientHeight)/2;
+  timelineMarker.style.marginTop=topMargin+"px";
+}
+
+const moveMarker=(index)=>{
+  var oneYearWapperHeight=(document.querySelector(".year-wrapper").clientHeight);
+  timelineMarker.style.top=index*oneYearWapperHeight + "px"
+}
+calculateTopMargin();
+window.onresize=calculateTopMargin;
 
 new fullpage("#fullpage",{
   autoScrolling:true,
@@ -10,15 +23,22 @@ new fullpage("#fullpage",{
   onLeave: (origin, destination, direction) => {
     if(destination.index===0){
       pageScrollButton.style.color = "#87e5ff";
+      moveMarker(destination.index);
     }
-    if(destination.index===1){
+    else if(destination.index===1){
       pageScrollButton.style.color = "#8DC8D9";
+      moveMarker(destination.index);
+
     }
-    if(destination.index===2){
+    else if(destination.index===2){
       pageScrollButton.style.color = "#43A3BD";
+      moveMarker(destination.index);
+
     }
-    if(destination.index===3){
+    else if(destination.index===3){
       pageScrollButton.style.color = "#6A97A3";
+      moveMarker(destination.index);
+
     }
   }
 });
