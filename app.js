34fe15b1f72c8b1c3a6dea8e83496f8edb2d3a25
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2021-10-04 11:54:21 +0530

    main scroll button addition
    added background, centered div and added scroll button

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
+const pageScrollButton = document.querySelector(".page-scroll-button");
+
 new fullpage("#fullpage",{
   autoScrolling:true,
-  navigation:true,
+  //navigation:true,
   fitToSection:true,
   marginLeft:"auto",
-  marginRight:"auto"
+  marginRight:"auto",
+  
+  onLeave: (origin, destination, direction) => {
+    if(destination.index===0){
+      pageScrollButton.style.color = "#87e5ff";
+    }
+    if(destination.index===1){
+      pageScrollButton.style.color = "#8DC8D9";
+    }
+    if(destination.index===2){
+      pageScrollButton.style.color = "#43A3BD";
+    }
+    if(destination.index===3){
+      pageScrollButton.style.color = "#6A97A3";
+    }
+  }
 });
+
+pageScrollButton.addEventListener("click",function(){
+  fullpage_api.moveSectionDown();
+})
